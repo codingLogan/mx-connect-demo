7f6af36826eb94ad04cbb6303c86e4cc2f920980
Logan Rasmussen <logan.rasmussen@mx.com>
2024-08-15 12:23:19 -0600

    Launch a new widget on each click of the buton
    This currently uses SFSafariViewController, but we may want to have the demo use WKWebView instead in order to interact with widget events.

--- clients/ios/ios/WidgetView.swift
+++ clients/ios/ios/WidgetView.swift
@@ -10,38 +10,69 @@ import SafariServices
 
 struct WidgetView: View {
     @State var showMXConnect = false
-    @State var urlString = "http://localhost:3000/"
+    @State var widgetUrl = ""
+    
+    func getWidgetUrl() async {
+        let url = URL(string: "http://localhost:3000/api/web_url")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedWidgetUrlResponse = try JSONDecoder().decode(WidgetUrlResponse.self, from: data)
+            widgetUrl = decodedWidgetUrlResponse.widget_url.url
+            showMXConnect = true
+        } catch {
+            print("Could not get widget URL: \(error.localizedDescription)")
+        }
+    }
     
     var body: some View {
         VStack {
             Text("Welcome to the MX Connect demo app")
             Button("Click to launch Connect in a webview") {
-                self.urlString = urlString
-                self.showMXConnect = true
-                print("Connect widget launch...")
+                Task {
+                    await getWidgetUrl()
+                }
             }
             .padding()
-            
-            // Open the URL
             .sheet(isPresented: $showMXConnect) {
-                SafariView(url:URL(string: self.urlString)!)
+                if (widgetUrl != "") {
+                    SafariSheetView(url:URL(string: widgetUrl)!)
+                } else {
+                    Text("Something went wrong with the Connect URL")
+                }
+            }
+            
+            if (widgetUrl != "") {
+                Text("Launched widget!")
             }
         }
     }
 }
 
-struct SafariView: UIViewControllerRepresentable {
+struct SafariSheetView: UIViewControllerRepresentable {
 
     let url: URL
 
-    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariSheetView>) -> SFSafariViewController {
         return SFSafariViewController(url: url)
     }
 
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
-
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariSheetView>) {
+        
     }
 
+}
+
+struct WidgetUrl: Codable {
+    var type: String
+    var url: String
+    var user_id: String
+}
+
+struct WidgetUrlResponse: Codable {
+    var widget_url: WidgetUrl
 }
 
 #Preview {
